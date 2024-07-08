018c0bf6a5085d215bf9d5c15d094ac4165a9ce9
Saad Iqbal <saad.j.iqbal@gmail.com>
2024-07-08 20:11:29 +1200

    Fixed warnings

--- scripts/pipes.gd
+++ scripts/pipes.gd
@@ -3,8 +3,8 @@ extends Area2D
 signal hit
 signal score
 
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	hit.emit()
 
-func _on_score_area_body_entered(body):
+func _on_score_area_body_entered(_body):
 	score.emit()
